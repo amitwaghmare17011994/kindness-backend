355f0ea46181e1bd307233f27e6d3d35ebd6c355
amitwaghmaretorinit <amit@torinit.ca>
2021-05-06 15:20:30 +0530

    route added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
- 
+
 
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-express()
-  .get('/', (req, res) => res.send('Hello'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => res.send('Hello'))
+
+app.get('/api/', (req, res) => res.send('api'))
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
