3220c37111fdbf7ea8e3b56087756c992861795c
Gajesh-LH <gajesh.panigrahi@livehealth.in>
2021-05-30 12:53:29 -0400

    Added api's for post.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import AuthController from '../controllers/auth.controller/index.js'
+import PostController from '../controllers/post.controller/index.js'
 import UserController from '../controllers/user.controller/index.js'
 import verify from './auth.js'
 
@@ -11,9 +12,9 @@ Router.get('/', (req, res) => res.send('Welcome!'))
 //auth routes
 Router.get('/user/:id', verify, UserController.getUserById)
 Router.get('/user/', verify, UserController.getUser)
-//auth routes
-
-Router.get('/any-query/', verify, UserController.getData)
+Router.get('/post/:id', verify, PostController.getPostById)
+Router.get('/post/', verify, PostController.getAllPost)
+Router.get('/any-query/', UserController.getData)
 Router.post('/login/', AuthController.login)
 Router.post('/user-register/', UserController.registerUser)
 
